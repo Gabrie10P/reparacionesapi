a1089b5c6767e948cce3c66c9ab8388cb629c766
Gabrie10P <gabrielpenate8@gmail.com>
2023-04-29 22:43:43 -0600

    update
    actualizacion

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const boduParser = require('body-parser')
 const fichas = require('./api/routes/fichas')
 
 const app = express()
 
-app.use(express.json())
-app.set('port',process.env.PORT | 3022)
+// app.use(express.json())
+
+app.use(boduParser.json())
 
 mongoose.connect('mongodb+srv://gabrielpenate8:Ay87unjInmlQyfVJ@reparaciones.kh0murh.mongodb.net/?retryWrites=true&w=majority')
 
@@ -15,6 +17,7 @@ app.post('/', fichas.create)
 app.put('/:id', fichas.update)
 app.delete('/:id', fichas.destroy)
 
-app.listen(app.get('port'), () =>{
-    console.log("Arrancando api");
+let PORT = 3022
+app.listen(PORT, () =>{
+    console.log(`Arrancando api en puerto ${PORT}`);
 })
